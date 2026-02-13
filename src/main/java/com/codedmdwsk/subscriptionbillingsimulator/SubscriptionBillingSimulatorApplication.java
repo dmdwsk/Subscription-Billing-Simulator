ef8731ca1b38881263556fbf5a8c3a6475f4dc4c
dmdwsk <dmdwsk200@gmail.com>
2026-02-13 22:12:36 +0100

    first commit

---
+++ src/main/java/com/codedmdwsk/subscriptionbillingsimulator/SubscriptionBillingSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.codedmdwsk.subscriptionbillingsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionBillingSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionBillingSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codedmdwsk/subscriptionbillingsimulator/TestSubscriptionBillingSimulatorApplication.java
@@ -0,0 +1,11 @@
+package com.codedmdwsk.subscriptionbillingsimulator;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestSubscriptionBillingSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(SubscriptionBillingSimulatorApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
