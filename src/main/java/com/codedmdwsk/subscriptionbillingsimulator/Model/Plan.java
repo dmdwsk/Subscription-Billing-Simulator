c2d3aef3d5d7d949a5b19546c4206be206f42c01
dmdwsk <dmdwsk200@gmail.com>
2026-02-14 15:41:11 +0100

    Finished Plan

--- src/main/java/com/codedmdwsk/subscriptionbillingsimulator/Model/Plan.java
+++ src/main/java/com/codedmdwsk/subscriptionbillingsimulator/Model/Plan.java
@@ -1,4 +1,36 @@
 package com.codedmdwsk.subscriptionbillingsimulator.Model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "plan")
 public class Plan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false,length = 255)
+    private String name;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private PlanPeriod period;
+    @Column(nullable = false,precision = 19,scale = 4)
+    private BigDecimal price;
+    @Column(nullable = false)
+    private Boolean active = true;
+    @Column(nullable = false)
+    private Instant createdAt;
+    @Column(nullable = false, length = 3)
+    private String currency;
+    @PrePersist
+    void onCreate() {
+        this.createdAt = Instant.now();
+        if (active == null) active = true;
+    }
 }
---
+++ src/main/java/com/codedmdwsk/subscriptionbillingsimulator/Model/PlanPeriod.java
@@ -0,0 +1,5 @@
+package com.codedmdwsk.subscriptionbillingsimulator.Model;
+
+public enum PlanPeriod {
+    MONTHLY, YEARLY
+}
