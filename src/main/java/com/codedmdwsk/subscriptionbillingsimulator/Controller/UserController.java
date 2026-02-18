a4c4012552b5bae0a76fa894f333f60d35276bca
dmdwsk <dmdwsk200@gmail.com>
2026-02-19 00:07:10 +0100

    Finished User part

--- src/main/java/com/codedmdwsk/subscriptionbillingsimulator/Controller/UserController.java
+++ src/main/java/com/codedmdwsk/subscriptionbillingsimulator/Controller/UserController.java
@@ -1,11 +1,9 @@
 package com.codedmdwsk.subscriptionbillingsimulator.Controller;
 
 import com.codedmdwsk.subscriptionbillingsimulator.Service.UserService;
-import com.codedmdwsk.subscriptionbillingsimulator.dto.UserCreateDto;
-import com.codedmdwsk.subscriptionbillingsimulator.dto.UserResponseDto;
+import com.codedmdwsk.subscriptionbillingsimulator.dto.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +19,30 @@ public class UserController {
     public List<UserResponseDto> getAllUsers(){
         return userService.getAllUsers();
     }
+    @GetMapping("/{id}")
+    public UserResponseDto getById(@PathVariable Integer id){
+        return userService.getById(id);
+    }
+    @PutMapping("/{id}")
+
+    public UserResponseDto update(@PathVariable Integer id, @Valid @RequestBody UserUpdateDto dto){
+        return userService.update(id,dto);
+    }
+    @PatchMapping("/{id}/password")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void changePassword(@PathVariable Integer id, @Valid @RequestBody ChangePasswordDto dto){
+        userService.changePassword(id,dto);
+    }
+    @PatchMapping("/{id}/role")
+    public UserResponseDto updateRole(@PathVariable Integer id, @Valid @RequestBody UpdateUserRoleDto dto){
+        return userService.updateRole(id,dto);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id) {
+        userService.delete(id);
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UserResponseDto create(@Valid @RequestBody UserCreateDto userCreateDto){
